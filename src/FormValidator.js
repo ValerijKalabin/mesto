c0cb1b580745ecc443c767f41e81f923dd8c855c
Valerij Kalabin <valerijkalabin@yandex.ru>
2020-09-08 12:56:03 +0700

    refactor: create FormValidator class

---
+++ src/FormValidator.js
@@ -0,0 +1,65 @@
+export default class FormValidator
+{
+  constructor(selectors, formElement) {
+    this._inputSelector = selectors.inputSelector;
+    this._submitButtonSelector = selectors.submitButtonSelector;
+    this._inactiveButtonClass = selectors.inactiveButtonClass;
+    this._inputErrorClass = selectors.inputErrorClass;
+    this._errorClass = selectors.errorClass;
+    this._formElement = formElement;
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._setEventListeners();
+    });
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+    this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
+    this._toggleButtonState();
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  _toggleButtonState() {
+    if(this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.removeAttribute('disabled');
+    }
+  }
+
+  _hasInvalidInput () {
+    return this._inputList.some(inputElement => !inputElement.validity.valid);
+  }
+
+  _checkInputValidity(inputElement) {
+    const errorElement = inputElement.nextElementSibling;
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, errorElement);
+    } else {
+      this._hideInputError(inputElement, errorElement);
+    }
+  }
+
+  _showInputError(inputElement, errorElement) {
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.classList.add(this._errorClass);
+    errorElement.textContent = inputElement.validationMessage;
+  }
+
+  _hideInputError(inputElement, errorElement) {
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = '';
+  }
+}
