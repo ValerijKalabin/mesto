03a656efcebe476d804ea336ad253702df831dae
Valerij Kalabin <valerijkalabin@yandex.ru>
2020-09-22 11:38:46 +0700

    feat: create a UserInfo class

---
+++ src/components/UserInfo.js
@@ -0,0 +1,20 @@
+export default class UserInfo
+{
+  constructor({titleSelector, subtitleSelector}) {
+    this._title = document.querySelector(titleSelector);
+    this._subtitle = document.querySelector(subtitleSelector);
+  }
+
+  getUserInfo() {
+    const userInfo = {
+      titleText: this._title.textContent,
+      subtitleText: this._subtitle.textContent
+    };
+    return userInfo;
+  }
+
+  setUserInfo({titleText, subtitleText}) {
+    this._title.textContent = titleText;
+    this._subtitle.textContent = subtitleText;
+  }
+}
